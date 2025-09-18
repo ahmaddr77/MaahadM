44844be5b062303430115860a55c0b607d95ffaf
ahmaddr77 <ahmadalb20025@gmail.com>
2025-09-18 15:16:44 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+{
+  "name": "معهد تعلم الألمانية",
+  "short_name": "معهد",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "description": "تطبيق ويب تفاعلي لتعلم اللغة الألمانية: دروس، أنشطة، واختبارات مع مفضلة للطلاب.",
+  "icons": [
+    {
+      "src": "https://assets.berty.tech/favicon_blueprint-android-chrome-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://upload.wikimedia.org/wikipedia/commons/3/3e/Android_robot_head.svg",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
